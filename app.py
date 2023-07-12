9c77c89da27b6c5d08c20e8507fc353f89763be1
ahmed.ghariani <ahmed.ghariani@etudiant-fst.utm.tn>
2023-07-12 16:05:14 +0100

    csv add read

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-import yaml, json
+import yaml, json, csv
 import logging
 from flask import Flask
 from flask_mqtt import Mqtt
-from flask_sqlalchemy import SQLAlchemy
 
-
-db = SQLAlchemy()
 app = Flask(__name__)
 app.config.from_file("config.yml", load=yaml.full_load)
 cfg = app.config
@@ -21,16 +18,33 @@ app_logger.name = "mqtt"  # change the logger name
 logging.getLogger("werkzeug").setLevel(cfg["WEBSERVER_LOGGING_LEVEL"])
 logging.info("app started")
 mqttc = Mqtt(app, mqtt_logging=cfg["MQTT_LOGGING"])
+conact_list = []
+with open(cfg["CONTACT_FILE"], newline="") as f:
+    for contact in csv.DictReader(f):
+        conact_list.append(contact)
 
 
+def get_contact(contact_name):
+    for contact in conact_list:
+        if contact["nom"] == contact_name:
+            break
+    if contact["nom"] == "default":
+        app_logger.info(
+            "contact name '" + contact_name + "' not found using the default number"
+        )
+    return contact
 
 
-@mqttc.on_connect()
-def on_connect(client, userdata, flags, rc):
-    app_logger.info("Connected with result code " + str(rc))
-    mqttc.subscribe("/nagios/cmdApp")
-    mqttc.subscribe("/nagios/pingApp",2)
-    mqttc.subscribe("/gsm/resp")
+def add_contcat(name, number):
+    contact = get_contact(name)
+    if contact["nom"] != "default":
+        app_logger.error("contact exist already " + str(contact))
+    else:
+        with open(cfg["CONTACT_FILE"], newline="", mode="w") as f:
+            writer = csv.DictWriter(f, ["nom", "tel_num"])
+            writer.writeheader()
+            writer.writerow({"nom": name, "tel_num": number})
+            writer.writerows(conact_list)
 
 
 def handle_gateway_responce(r):
@@ -46,7 +60,7 @@ def handle_gateway_responce(r):
 def handel_notification(n):
     if not (n["State"] in ["CRITICAL", "DOWN", "WARNING"]):
         return
-    num = "+21694960307"  # get_contact_number(n["Name"])
+    num = get_contact(n["Name"])["tel_num"]
     name = n.pop("Name")
     m = {"Msg": str(n).replace("{", "").replace("}", "").replace("'", "")}
     n["Name"] = name
@@ -59,6 +73,13 @@ def handel_notification(n):
     mqttc.publish("/gsm/cmd1", json.dumps(m).encode("utf-8"))
     mqttc.publish("/gsm/cmd2", json.dumps(m).replace('"', '\\"').encode("utf-8"))
 
+
+@mqttc.on_connect()
+def on_connect(client, userdata, flags, rc):
+    app_logger.info("Connected with result code " + str(rc))
+    mqttc.subscribe("/nagios/cmdApp")
+    mqttc.subscribe("/nagios/pingApp", 2)
+    mqttc.subscribe("/gsm/resp")
 
 
 @mqttc.on_message()
