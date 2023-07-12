7e5996747e41265b520cb8a460c0a2a1471fe5d3
ahmed.ghariani <ahmed.ghariani@etudiant-fst.utm.tn>
2023-07-12 12:08:58 +0100

    mqtt running*

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
-import yaml
+import yaml, json
 import logging
 from flask import Flask
 from flask_mqtt import Mqtt
+from flask_sqlalchemy import SQLAlchemy
 
+
+db = SQLAlchemy()
 app = Flask(__name__)
 app.config.from_file("config.yml", load=yaml.full_load)
 cfg = app.config
@@ -13,7 +16,61 @@ logging.basicConfig(
     level=cfg["LOGGING_LEVEL"],
 )
 gsm_logger = logging.getLogger("gsm")
-logging.getLogger("flask_mqtt").name = "mqtt"  # change the logger name
-logging.getLogger("werkzeug").setLevel(logging.WARNING)
+app_logger = logging.getLogger("flask_mqtt")
+app_logger.name = "mqtt"  # change the logger name
+logging.getLogger("werkzeug").setLevel(cfg["WEBSERVER_LOGGING_LEVEL"])
 logging.info("app started")
 mqttc = Mqtt(app, mqtt_logging=cfg["MQTT_LOGGING"])
+
+
+
+
+@mqttc.on_connect()
+def on_connect(client, userdata, flags, rc):
+    app_logger.info("Connected with result code " + str(rc))
+    mqttc.subscribe("/nagios/cmdApp")
+    mqttc.subscribe("/nagios/pingApp",2)
+    mqttc.subscribe("/gsm/resp")
+
+
+def handle_gateway_responce(r):
+    state = r.split(",")[0]
+    if state == "WR":
+        gsm_logger.warning(r)
+    elif state == "CR":
+        gsm_logger.critical(r)
+    else:
+        gsm_logger.info(r)
+
+
+def handel_notification(n):
+    if not (n["State"] in ["CRITICAL", "DOWN", "WARNING"]):
+        return
+    num = "+21694960307"  # get_contact_number(n["Name"])
+    name = n.pop("Name")
+    m = {"Msg": str(n).replace("{", "").replace("}", "").replace("'", "")}
+    n["Name"] = name
+    n["Num"] = num
+    n["Vocal"] = n["State"] in ["CRITICAL", "DOWN"]
+    m["Num"] = "+216" + num
+    m["Name"] = name
+    m["Vocal"] = n["Vocal"]
+    mqttc.publish("/gsm/cmd", json.dumps(n).encode("utf-8"))
+    mqttc.publish("/gsm/cmd1", json.dumps(m).encode("utf-8"))
+    mqttc.publish("/gsm/cmd2", json.dumps(m).replace('"', '\\"').encode("utf-8"))
+
+
+
+@mqttc.on_message()
+def on_message(client, userdata, msg):
+    t = msg.topic
+    if t == "/nagios/pingApp":
+        client.publish("/nagios/pongApp", "pong")
+    elif t == "/nagios/cmdApp":
+        try:
+            n = json.loads(msg.payload)
+            handel_notification(n)
+        except json.decoder.JSONDecodeError:
+            app_logger.warning("bad format")
+    elif t == "/gsm/resp":
+        handle_gateway_responce(msg.payload.decode("utf-8"))
